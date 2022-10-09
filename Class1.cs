36ed00cad1ef09ae8c5d0f703058fbf7a171d9b6
ashwin333 <ashwinpatil832@gmail.com>
2022-10-09 22:50:20 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeedemo
+{ 
+    internal class Employee
+    {
+        private int id;
+        private string name;
+        double salary;
+
+        public void add(int id, string name, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+        public string Print()
+        {
+            return $" Employee:{id}/{name}/{salary}";
+        }
+    } 
+}     
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeedemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            e1.add(93, "Ashwin", 75.7);
+            Console.WriteLine(e1.Print());  
+        }
+    }
+}
